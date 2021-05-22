bc5f27b04f92e0baba584633f399f4fd17d66e5a
jambanhanyut <colonelmacmillan@gmail.com>
2021-05-22 10:03:31 +0700

    add interval, add restartbutton

--- games/dakon.js
+++ games/dakon.js
@@ -27,32 +27,55 @@ function gameBegin() {
         if (i < 8) {
             if (i != 7) {
                 allHoles.push(new holes(rads, "red", xh, yh, 7));
+                allHoles[i].update();
                 xh += 50;
             } else {
                 // Hole [7] is the enemy Bank hole
                 yh = 100;
                 allHoles.push(new holes(radb, "red", xh, yh, 0));
+                allHoles[i].update();
                 xh = 100;
                 yh = 150;
             }
         } else {
             if (i != 15) {
                 allHoles.push(new holes(rads, "blue", xh, yh, 7));
+                allHoles[i].update();
                 xh += 50;
             } else {
                 // Hole [15] is the our Bank hole
                 xh = 50;
                 yh = 100;
                 allHoles.push(new holes(radb, "blue", xh, yh, 0));
+                allHoles[i].update();
             }
         }
     }
 }
 
+// Update Area
+function updateArea(idx) {
+    // What happend every interval 20
+    allHoles[idx].update();
+}
+
 // One turn
 function oneTurn(idx) {
     // One turn function here
+    setInterval(updateArea(idx), 20);
+}
 
+// Enemy's turn
+// enhanced with AI
+function enemyTurn(idx) {
+    const button = document.querySelectorAll("button");
+    button.disabled = true;
+
+    // Enemy's turn function here
+
+    setInterval(updateArea(idx), 20);
+
+    button.disabled = false;
 }
 
 // Hole component
@@ -61,6 +84,7 @@ function holes(rad, color, x, y, num) {
     this.x = x;
     this.y = y;
     this.num = num;
+    this.oldnum = this.num;
 
     // Circle shape
     ctx.fillStyle = color;
@@ -68,15 +92,6 @@ function holes(rad, color, x, y, num) {
     ctx.arc(this.x, this.y, this.rad, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
-
-    // Text in the circle
-    ctx.fillStyle = "white";
-    ctx.font = "20px Arial";
-    if (this.num < 10) {
-        ctx.fillText(this.num, this.x - 5, this.y + 7);
-    } else {
-        ctx.fillText(this.num, this.x - 12, this.y + 7);
-    }
 
     // Add num function
     this.addNum = function () {
@@ -86,9 +101,38 @@ function holes(rad, color, x, y, num) {
     this.clearNum = function () {
         this.num = 0;
     }
+    // Update shape and num
+    this.update = function () {
+        // Add shape
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.rad, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+
+        // Add new text
+        ctx.fillStyle = "white";
+        ctx.font = "20px Arial";
+        if (this.num < 10) {
+            ctx.fillText(this.num, this.x - 5, this.y + 7);
+        } else {
+            ctx.fillText(this.num, this.x - 12, this.y + 7);
+        }
+    }
 }
 
+// Function for restart
+function restart() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    allHoles = [];
+    gameBegin();
+}
+
+// Play function
 function tap(idx) {
     // Play one turn here
     oneTurn(idx);
+    enemyTurn();
+    // allHoles[idx].addNum();
+    // allHoles[idx].update();
 }
